56d4051819e637277d04d6fce212df83d37c1da3
Zaka Salah Eddine <zakasalaheddine@gmail.com>
2021-01-11 17:36:09 +0100

    update run scripts

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000
   },
   module: {
     rules: [
